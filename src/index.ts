e66e3fa74afa1809361ea0fa6206f1ad7c2f0625
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-05 06:55:18 -0300

    feat: ensure google sign in always prompt for account seletion

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ await app.register(fastifySwagger, {
     servers: [
       {
         description: "localhost",
-        url: "http://localhost:3000",
+        url: "http://localhost:8080",
       },
     ],
   },
@@ -146,7 +146,7 @@ app.route({
 });
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 3000 });
+  await app.listen({ port: Number(process.env.PORT) || 8080 });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
